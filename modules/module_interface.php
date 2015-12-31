ac8339a6dfda3a082240a75a84f1998be539ee00
Paul Sohier <paul999@phpbb.com>
2015-12-31 21:47:40 +0100

    Tabs

--- modules/module_interface.php
+++ modules/module_interface.php
@@ -12,82 +12,82 @@ namespace paul999\tfa\modules;
 
 interface module_interface
 {
-    /**
-     * Return if this module is enabled by the admin
-     * (And all server requirements are met).
-     *
-     * Do not return false in case a specific user disabeld this module,
-     * OR if the user is unable to use this specific module.
-     * @return boolean
-     */
-    public function is_enabled();
+	/**
+	 * Return if this module is enabled by the admin
+	 * (And all server requirements are met).
+	 *
+	 * Do not return false in case a specific user disabeld this module,
+	 * OR if the user is unable to use this specific module.
+	 * @return boolean
+	 */
+	public function is_enabled();
 
-    /**
-     * Check if the current user is able to use this module.
-     *
-     * This means that the user enabled it in the UCP,
-     * And has it setup up correctly.
-     * This method will be called during login, not during registration/
-     *
-     * @param int $user_id
-     * @return bool
-     */
-    public function is_usable($user_id);
+	/**
+	 * Check if the current user is able to use this module.
+	 *
+	 * This means that the user enabled it in the UCP,
+	 * And has it setup up correctly.
+	 * This method will be called during login, not during registration/
+	 *
+	 * @param int $user_id
+	 * @return bool
+	 */
+	public function is_usable($user_id);
 
-    /**
-     * Check if the user can potentially use this.
-     * This method is called at registration page.
-     *
-     * You can, for example, check if the current browser is suitable.
-     *
-     * @param int $user_id
-     * @return bool
-     */
-    public function is_potentially_usable($user_id);
+	/**
+	 * Check if the user can potentially use this.
+	 * This method is called at registration page.
+	 *
+	 * You can, for example, check if the current browser is suitable.
+	 *
+	 * @param int $user_id
+	 * @return bool
+	 */
+	public function is_potentially_usable($user_id);
 
-    /**
-     * Get the priority for this module.
-     * A lower priority means more chance it gets selected as default option
-     *
-     * There can be only one module with a specific priority!
-     * If there is already a module registered with this priority,
-     * a Exception might be thrown
-     *
-     * @param int $user_id If set, the priority can depend on the current user
-     * @return int
-     */
-    public function get_priority($user_id = 0);
+	/**
+	 * Get the priority for this module.
+	 * A lower priority means more chance it gets selected as default option
+	 *
+	 * There can be only one module with a specific priority!
+	 * If there is already a module registered with this priority,
+	 * a Exception might be thrown
+	 *
+	 * @param int $user_id If set, the priority can depend on the current user
+	 * @return int
+	 */
+	public function get_priority($user_id = 0);
 
-    /**
-     * Start of the login procedure.
-     * @param int $user_id
-     * @return int
-     */
-    public function login_start($user_id);
+	/**
+	 * Start of the login procedure.
+	 * @param int $user_id
+	 * @return int
+	 */
+	public function login_start($user_id);
 
-    /**
-     * Actual login procedure
-     * @param int $user_id
-     */
-    public function login($user_id);
+	/**
+	 * Actual login procedure
+	 * @param int $user_id
+	 */
+	public function login($user_id);
 
-    /**
-     * Start of registration
-     * @return void
-     */
-    public function register_start();
+	/**
+	 * Start of registration
+	 * @return void
+	 */
+	public function register_start();
 
-    /**
-     * Actual registration
-     * @return void
-     */
-    public function register();
+	/**
+	 * Actual registration
+	 * @return void
+	 */
+	public function register();
 
-    /**
-     * This method is called to show the UCP page.
-     * You can assign template variables to the template, or do anything else here.
-     */
-    public function show_ucp();
+	/**
+	 * This method is called to show the UCP page.
+	 * You can assign template variables to the template, or do anything else here.
+	 */
+	public function show_ucp();
 
 	/**
 	 * Delete a specific row from the UCP.
